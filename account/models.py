0c06b764cc56ea2a1a3d19abc317efdbc28894f2
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-07-16 12:51:36 +0430

    remove profile model and move the fileds to custom user model

--- account/models.py
+++ account/models.py
@@ -1,31 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import User,AbstractUser
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
+    name = models.CharField(max_length=255,blank=True)
+    picture = models.ImageField(upload_to='profile_pics',blank=True,null=True)
+    location = models.CharField(max_length=50,blank=True,null=True)
+    bio = models.CharField(max_length=280,blank=True,null=True)
+    date_of_birth = models.DateField(null=True)
     following = models.ManyToManyField('self',
                                        through='Contact',
                                        related_name='followers',
                                        symmetrical=False)
 
-
-class Profile(models.Model):
-
-    user = models.ForeignKey(to=get_user_model(),on_delete=models.CASCADE,
-                             related_name='profile',null=False)
-    bio = models.TextField(max_length=200,blank=True)
-    date_of_birth = models.DateField(blank=True,null=True)
-    # photo = models.ImageField(blank=True,upload_to='avatars')
-
     def __str__(self):
-        return "{}'s Profile".format(self.user.username)
+        return self.username
+
+    def get_absolute_url(self):
+        return reverse('account:details',kwargs={'username':self.username})
 
 
 class Contact(models.Model):
-    from_user = models.ForeignKey(to=get_user_model(),
+    from_user = models.ForeignKey(to=User,
                                   on_delete=models.CASCADE,related_name='rel_from_set')
-    to_user = models.ForeignKey(to=get_user_model(),
+    to_user = models.ForeignKey(to=User,
                                 on_delete=models.CASCADE,related_name="rel_to_set")
     created = models.DateTimeField(auto_now_add=True,db_index=True)
 
