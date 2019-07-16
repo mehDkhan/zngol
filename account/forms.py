f6327c499598e8c944c788cc37de87646ba6d342
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-07-16 12:55:48 +0430

    remove unnecessary lines

--- account/forms.py
+++ account/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from .models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('following',)
+        model = User
+        fields = UserCreationForm.Meta.fields
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
+        model = User
         fields = UserChangeForm.Meta.fields
