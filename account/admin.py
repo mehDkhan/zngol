f9fea9954d1d09431a8fc0fdd874cf48bf1c4989
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-07-16 12:54:04 +0430

    customizing admin for User

--- account/admin.py
+++ account/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import User
 from .forms import CustomUserChangeForm,CustomUserCreationForm
 
 
-class CustomUserAdmin(UserAdmin):
+class UserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = CustomUser
+    model = User
+    fieldsets = (
+            ('User Profile', {'fields': ('name',)}),
+    ) + UserAdmin.fieldsets
+    list_display = ('username','name','is_superuser')
+    search_fields = ['name',]
 
 
-admin.site.register(CustomUser,CustomUserAdmin)
+admin.site.register(User,UserAdmin)
