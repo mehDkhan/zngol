743b502a476505d1b8694c851d82bfbd2ceb9df3
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-07-16 12:54:58 +0430

    change account detail view name to details

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ app_name = 'account'
 
 urlpatterns= [
     path('accounts/register',views.register,name='register'),
-    re_path(r'^account/(?P<username>\w+)$',views.profile,name='account'),
+    re_path(r'^account/(?P<username>\w+)$',views.DetailView.as_view(),name='details'),
     path('', include('django.contrib.auth.urls')),
 
 ]
