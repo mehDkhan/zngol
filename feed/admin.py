bb8cfe97a0515b187d96cb677fe8ffd9c9f8e0c0
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-07-30 18:50:39 +0430

    remove some fileds from post model

--- feed/admin.py
+++ feed/admin.py
@@ -4,13 +4,12 @@ from .models import Post,Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','slug','author','publish','status')
-    list_filter = ('status','created','publish','author')
+    list_display = ('title','slug','author')
+    list_filter = ('created','author')
     search_fields = ['title','body']
     prepopulated_fields = {'slug':('title',)}
     raw_id_fields = ('author',)
-    date_hierarchy = 'publish'
-    ordering = ('status','publish')
+    ordering = ('created','updated')
 
 
 @admin.register(Comment)
--- feed/models.py
+++ feed/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from account.models import User
 from django.utils import timezone
+from django.utils.text import slugify
 
 
 class Post(models.Model):
-    STATUS = (('draft','Draft'),('published','Published'))
 
     author = models.ForeignKey(to=User,
                                on_delete=models.SET_NULL,
@@ -14,17 +14,19 @@ class Post(models.Model):
     title = models.CharField(max_length=140,blank=False,null=False)
     body = models.TextField(max_length=250,blank=False,null=False)
     slug = models.SlugField(max_length=250,unique_for_date='publish')
-    publish = models.DateTimeField(default=timezone.now())
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=10, choices=STATUS, default='draft')
 
     class Meta:
-        ordering = ('-publish',)
+        ordering = ('-created','-updated')
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 
 class Comment(models.Model):
